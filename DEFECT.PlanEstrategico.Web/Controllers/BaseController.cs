4a560132c77b2d771465fdd8760659b1caced071
Nicolás Giannetti <juannetti@gmail.com>
2017-04-22 14:13:54 -0300

    first commit

---
+++ DEFECT.PlanEstrategico.Web/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using DEFECT.PlanEstrategico.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DEFECT.PlanEstrategico.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        public DEFECTPlanEstrategicoDataContext db = new DEFECTPlanEstrategicoDataContext("AppDb")
+            .WithConnectionStringFromConfiguration();
+        //// GET: Base
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ DEFECT.PlanEstrategico.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DEFECT.PlanEstrategico.Web.MvcApplication" Language="C#" %>
